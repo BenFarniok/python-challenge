4cd1d15c11bb30689e2ba35f5a86a61758b53f36
Ben F <benfarniok@gmail.com>
2020-03-30 16:41:22 -0500

    updating homework to clean it up a bit, still works

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,14 +1,12 @@
 import os
 import csv
 
-winner = []
-votenums= []
 candidates = []
 candidate_list  = []
 total_votes = 0
 total_vote = 0
-vote_list =[]
 votes = 0
+vote_list =[]
 votes_dict = {}
 vote_values= []
 percentage = 0
@@ -17,7 +15,7 @@ winner_score = 0
 winner = []
 value=0
 j=0
-percent = []
+
 csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, newline='') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
@@ -26,7 +24,6 @@ with open(csvpath, newline='') as csvfile:
         csv_header = next(csvreader)
     #votes = [row[2]]
         for row in csvreader:
-            total_votes += 1
             #make list of candidates/votes
             candidates.append(row[2])
             #make variable to hold candidate names from candidates list
@@ -45,21 +42,23 @@ with open(csvpath, newline='') as csvfile:
            #convert values of dict into a list 
         vote_values = list(votes_dict.values())
         votes = vote_values[j]
+        #find total vote
+        total_vote = sum(vote_values)
         for j in range(len(vote_values)):
             value=int(vote_values[j])
             #find who had the largest amount of votes
             if value > winner_score:
                 winner_score=vote_values[j]
                 winner = candidate_list[j]
-            percentage = (vote_values[j]/total_votes)*100
+            #calculate percentage    
+            percentage = (vote_values[j]/total_vote)*100
+            #format percentage and add to list
             if percentage not in percentage_list:
                 percentage ="{:.3}%".format(percentage)
                 percentage_list.append(percentage)
-            j=j+1  
-        total_vote = sum(vote_values)
+            j=j+1
 
-  #* The winner of the election based on popular vote.
-#((/total_votes)*100)
+
 print("Election Results")
 print("--------------------")
 print("Total votes: " + str(total_vote))
